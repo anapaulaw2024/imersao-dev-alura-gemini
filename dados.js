f24ad2528a5fc79fd80fbb04d910d2dabc0475db
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-02-05 18:05:24 +0000

    Atualizar o dados.js

--- dados.js
+++ dados.js
@@ -1,31 +1,34 @@
-let autores = [
+let autoresRomance = 
   {
     genero: "romance",
     nome: "Itamar Vieira Junior, Geovani Martins, Djamila Ribeiro, Bruna Beber, Marcelino Freire",
     descricao: "Aprofunde em: https://www.skoob.com.br/, https://www.submarino.com.br/, https://www.intrinseca.com.br/"
-  },
+  };
+let autoresFantasia =
   {
     genero: "fantasia",
     nome: "Aline Valek, André Vianco, Roberta Estrela, Marcelo Cassaro",
     descricao: "Aprofunde em: https://www.skoob.com.br/, https://www.submarino.com.br/, https://www.intrinseca.com.br/, https://www.leya.com/"
-  },
+  };
+let autoresFiccao =
 
   {
     genero: "ficção",
     nome: "Julián Fuks, Daniel Galera, Antonio Xerxenesky, Rafael Montes, Bruna Tessari",
     descricao: "Aprofunde em em:https://www.skoob.com.br/ , https://www.submarino.com.br/ , https://www.intrinseca.com.br/ ,https://www.companhiadasletras.com.br/"
-  },
+  };
+let autoresContos =
   
   {
     genero: "contos e crônicas",
     nome: "Ana Paula Maia, Rodrigo Lacerda, Tatiana Salem Levy, Gregório Duvivier",
     descricao: "Aprofunde em:https://www.skoob.com.br/ , https://www.submarino.com.br/ ,  https://www.intrinseca.com.br/ ,   https://www.leya.com/"
-  },
+  };
+let autoresPoesia =
   
   {
     genero: "poesia",
     nome: "Ana Martins Marques, Tati Bernardi, Rodrigo Garcia Lopes",
     descricao: "Aprofunde em:https://www.skoob.com.br/ , https://www.submarino.com.br/ ,https://www.intrinseca.com.br/ ,    https://www.leya.com/"
 
-  }
-];
+  };
